d07b5206717331db57f906cac09d7f35b195bb67
ProfOmar <oaithel@yahoo.com>
2026-01-13 18:54:03 -0500

    1/8/2026

---
+++ src/com/mac190/secondorder/SecondOrderEquation.java
@@ -0,0 +1,20 @@
+package com.mac190.secondorder;
+/*
+Write a java program that solves a second order equation in the form
+aX^2 + bX + C = 0. a, b, and c coefficients are the inputs to your program.
+if a is 0, then we have a first order equation, two cases for b:
+    if b is 0, then we have two cases for c
+        if c is 0 then (0 = 0), which has infinite number of silutions
+        if c is not 0, then no solution
+    if b is not 0, then it's a first order equation with one solution -c/b.
+if a is not 0, then we compute D the dicriminant as D = b^2 - 4ac
+    We have three cases:
+    D is 0, then there is one double solution -b/2a
+    D is larger than 0,we have two distinct solutions -b +- squareroot of D /2a
+    D is negative, then no real solutions.
+ */
+public class SecondOrderEquation {
+    public static void main(String[] args) {
+
+    }
+}
