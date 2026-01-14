50958691ad88667e48c5e740af739263e4867225
ProfOmar <oaithel@yahoo.com>
2026-01-13 20:33:27 -0500

    1/8/2026

---
+++ src/com/mac190/introclasses/IntroClasses.java
@@ -0,0 +1,75 @@
+package com.mac190.introclasses;
+/*
+A class is a blueprint of objects, a description of objects in terms of data
+and actions that can be performed on that data. For instance, you can describe a car as
+A car is something that has a number of doors, a fuelType, VIN, number of seats,
+color, power .....what actions can you perform on an object car
+brake(), moveBack(), moveforward(), turnLeft(), trunRight() ....
+These actions act on the data of the car, an called methods.
+To define a class we use:
+public class ClassName{
+    AccessSpecifier  type  variableName;
+    AccessSpecifier  type  variableName2;
+
+
+    //methods
+    public type methodName(params){
+
+    }
+}
+AccessSpecifier can be
+public: anyone can access, no restriction
+private: only the class itself can access none else
+protected: the class itself and the derived classes can access the member (within the package).
+
+Example
+public class Sum{
+    int x, y;
+
+    public void add(){
+          System.out.println(x + " + " + y + " = " + (x+y));
+    }
+    public void subtract(){
+          System.out.println(x + " - " + y + " = " + (x-y));
+    }
+     public void multiply(){
+          System.out.println(x + " * " + y + " = " + (x*y));
+    }
+    public void divide(){
+          System.out.println(x + " / " + y + " = " + (x/y));
+    }
+   }
+
+   In order to create an object of a given class, we use
+   ClassName objectName = new ClassName();
+   //create an object Sum
+   Sum calc = new Sum(); //S has two integer attributes (attributes are values of the member variables
+   of an object)
+   //to access the member variables (attributes of an object), objectName.variableName
+   //assign 3 to x of calc
+   calc.x = 3;
+   calc.y = 5;
+   //invoke method add on object calc
+   calc.add();
+
+ */
+public class IntroClasses {
+    public static void main(String[] args) {
+        Sum calc = new Sum(); //S has two integer attributes (attributes are values of the member variablesof an object)
+        //to access the member variables (attributes of an object), objectName.variableName
+        //assign 3 to x of calc
+        calc.x = 3;
+        calc.y = 5;
+        //invoke method add on object calc
+        calc.add();
+        calc.multiply();
+        //create another Sum object with 2 and 4 for x and y
+
+        //invoke add and subtract on the new object.
+
+        //assign the old object tot he new object
+
+        //invoke add and subtract on the new object, and on the old object
+        //what do you think happened? How many objects do you have now?
+    }
+}
---
+++ src/com/mac190/introclasses/Sum.java
@@ -0,0 +1,18 @@
+package com.mac190.introclasses;
+
+public class Sum{
+    int x, y;
+
+    public void add(){
+        System.out.println(x + " + " + y + " = " + (x+y));
+    }
+    public void subtract(){
+        System.out.println(x + " - " + y + " = " + (x-y));
+    }
+    public void multiply(){
+        System.out.println(x + " * " + y + " = " + (x*y));
+    }
+    public void divide(){
+        System.out.println(x + " / " + y + " = " + (x/y));
+    }
+}
