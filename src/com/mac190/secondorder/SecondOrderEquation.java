012a061aa5cd16bca9cf6adc5d0e3b7bef69e274
ProfOmar <oaithel@yahoo.com>
2026-01-13 19:44:26 -0500

    1/8/2026

--- src/com/mac190/secondorder/SecondOrderEquation.java
+++ src/com/mac190/secondorder/SecondOrderEquation.java
@@ -1,4 +1,8 @@
 package com.mac190.secondorder;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /*
 Write a java program that solves a second order equation in the form
 aX^2 + bX + C = 0. a, b, and c coefficients are the inputs to your program.
@@ -15,6 +19,42 @@ if a is not 0, then we compute D the dicriminant as D = b^2 - 4ac
  */
 public class SecondOrderEquation {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        char userChoice = 'y';
+        do{
+        System.out.println("Please enter the coefficients a, b, and c: ");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        if(a == 0) {
+            if(b == 0){
+                if(c == 0){
+                    System.out.println("This is 0 = 0, infinite number of solutions");
+                }else{
+                    //c not 0
+                    System.out.println("C = 0, no solution");
+                }
+            }else{
+                //b is not 0
+                System.out.println("This is a first order equation solution: "+ (-c/b));
+            }
+        }else{
+            //a not 0
+            //compute the discriminant
+            double D = Math.pow(b, 2) - 4*a*c;
+            if(D == 0){
+                System.out.println("there is one double solution: " + (-b/(2*a)));
+            }else if(D > 0) {
+                double X1 = (-b + Math.sqrt(D))/(2*a);
+                double X2 = (-b - Math.sqrt(D))/(2*a);
+                System.out.println("there are two solutions: \nX1 = " + X1 + "\nX2 = " + X2);
+            }else{
+                //D < 0
+                System.out.println("there are no solutions");
+            }
+        }
+            System.out.println("Do you want to continue[Y/N]: ");
+            userChoice = sc.next().charAt(0);
+        }while(Character.toLowerCase(userChoice) == 'y');
     }
 }
