3b6ff0f0002c52b2145117a5fc4185ca300ae9fd
ProfOmar <oaithel@yahoo.com>
2026-01-20 20:06:01 -0500

    1/20/2026

---
+++ src/com/mac190/circle/Circle.java
@@ -0,0 +1,21 @@
+package com.mac190.circle;
+/*
+Design a class Circle that has the following:
+- A Point object center as a member variable
+- a double member variable for radius
+- setters getters
+- default constructor, center to default point and redius to 0
+- Constructor that accepts one double, sets center to default point and rediius to input
+- Constructor that accepts three doubles, the first two for the center and the last
+for radius.
+- Constructor that accepts a Point and a double, the point for center and the double
+for radius.
+- toString to return the Circle in the form {(x, y), radius}
+- equals to compare two Circle objects.
+- Method Circle add(Circle c), returns a new Circle object with the midpoint betweeb
+the two centers as the center and the average radii as the radius.
+- implement the method add above as a static method
+- Test both class extensively. Use all methods with appropriate parameters.
+ */
+public class Circle {
+}
---
+++ src/com/mac190/circle/Point.java
@@ -0,0 +1,16 @@
+package com.mac190.circle;
+/*
+Design and implement a class Point that has the following:
+- Two double member variables, coordinates X and Y
+- Setters/getters for each and setter for both (first for X, second forY)
+- Constructors: default, one that takes in one param for both X and Y
+and one constructor that accepts two parameter, first for X second for Y
+- Copy constructor
+- Override toString to return the point as a string in the form (x, y)
+- Override equals to compare two Points
+- Method double distance(Point p) returns the distance between this point and p
+- method Point add(Point p) returns the midpoint between this and p
+- implement the previous methods as static (both distance and add)
+ */
+public class Point {
+}
