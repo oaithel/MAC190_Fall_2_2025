dc569b8861bdd1efc1f802a8118d4202bd02b916
momo <oaithel@yahoo.com>
2026-01-29 18:44:03 -0500

    1/29/2026

---
+++ src/com/mac190/animal/Animal.java
@@ -0,0 +1,12 @@
+package com.mac190.animal;
+/*
+Design a class Animal that has the following:
+age, color, species, diet
+- setters getters
+- default constructor set age to 0 all strings to "N/A"
+- Constructor that accepts all attributes
+- toString, equals
+- method speak() display don't know how to speak yet!
+ */
+public class Animal {
+}
---
+++ src/com/mac190/animal/Dog.java
@@ -0,0 +1,13 @@
+package com.mac190.animal;
+/*
+Derive a class Dog from Animal, knowing that a Dog is an Animal
+and in addition it has, breed and its diet is Omnivore, and of Canine species.
+- setters getters
+- default constructor
+- constructor that accepts any attribute needed.
+- toString, equals
+- speak method displays "Waf Waf !".
+-Test your classes.
+ */
+public class Dog {
+}
