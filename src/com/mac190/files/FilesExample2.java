6ac808a9a339bf1d4bdba5c894f5445c309e9170
ProfOmar <oaithel@yahoo.com>
2026-02-10 20:35:30 -0500

    2/10/2026

--- src/com/mac190/files/FilesExample2.java
+++ src/com/mac190/files/FilesExample2.java
@@ -11,5 +11,37 @@ in a separate variable. the time, distance and date.
 
  */
 
+import java.util.Scanner;
+import java.io.*;
 public class FilesExample2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the file name:");
+        String fileName = sc.nextLine();
+        //create a File object
+        File inFile = new File(fileName);
+        if(!inFile.exists()){
+            System.out.println("The file does not exist");
+            System.exit(1);
+        }
+        try{
+            FileReader fr = new FileReader(inFile);
+            Scanner scanFile = new Scanner(fr);
+            while(scanFile.hasNext()){
+                String t = scanFile.next();
+                double time = scanFile.nextDouble();
+                String d = scanFile.next();
+                int distance = scanFile.nextInt();
+                String dt = scanFile.next();
+                String date = scanFile.nextLine();
+                System.out.println("input: " + t + " " + time + " " + d +" " + distance +" " + dt + " " + date);
+            }
+
+        }catch(IOException e){
+            System.out.println("Caught IOException: " + e.getMessage());
+        }catch(Exception e){
+            System.out.println("Exception: " + e.getMessage());
+        }
+
+    }
 }
