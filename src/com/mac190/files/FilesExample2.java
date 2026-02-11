e728661ad6b6770182bedc186161cb01169b67f2
ProfOmar <oaithel@yahoo.com>
2026-02-10 20:13:08 -0500

    2/10/2026

---
+++ src/com/mac190/files/FilesExample2.java
@@ -0,0 +1,15 @@
+package com.mac190.files;
+
+/*
+Using notepad, create a file nam as you want.
+Wite the folowing data to the file:
+
+time 34.56  distance 20  date 2/3/2026
+
+//write a program that opens the file and reads eachof the following
+in a separate variable. the time, distance and date.
+
+ */
+
+public class FilesExample2 {
+}
